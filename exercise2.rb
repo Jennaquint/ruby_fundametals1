64eb230de80a3796a242ed9484b4fe56de046207
jonssmall <jonssmall@gmail.com>
2014-01-14 13:32:36 -0500

    exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+#1. How would you calculate the tip for a 55 dollar meal?
+puts 55 * 0.15 
+
+#2. Try adding a string to an integer.
+puts 2.to_s + "5"
+
+#3. Output the result of multiplying 45628 by 7839 in a sentence using string interpolation :
+multiply = 45628 * 7839
+puts "The is the answer #{multiply}"
+
+#4. What's the value of the expression (true && false) || (false && true) || !(fasle && false)
+puts true
